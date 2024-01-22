bdb8638a2facfa97db4142012a5ead51cb540674
tumero58 <erik.tumasyan@inomma.com>
2024-01-22 10:01:36 +0400

    feat: add active item

--- src/components/ItemsWrapper.tsx
+++ src/components/ItemsWrapper.tsx
@@ -1,26 +1,42 @@
+import { useState } from "react";
 import DragItem from "./DragItem";
 
 const ItemsWrapper = ({ currentItems, setItems }: any) => {
+    const [activeItem, setActiveItem] = useState<any>(currentItems?.[0] || {});
+    const handleItemClick = (item: any) => {
+        setActiveItem(item);
+    };
     return (
         <div style={{
             width: "100%",
-            height: "100%"
+            height: "100%",
+            position: "relative",
+            paddingTop: "20px"
         }}>
             <div style={{
                 display: "flex",
-                gap: "16px"
+                gap: "16px",
+                position: "absolute",
+                top: 0,
+                left: 0
             }}>
                 {currentItems?.map((item: any, index: number) => (
-                    <DragItem
-                        key={item.id}
-                        name={item.name}
-                        currentPositionName={item.position}
-                        setItems={setItems}
-                        index={index}
-                        component={item.component}
-                    />
+                    <div key={item.id} style={{
+                        background: item.id === activeItem?.id ? "yellow" : "white"
+                    }} onClick={() => {
+                        handleItemClick(item);
+                    }}>
+                        <DragItem
+                            name={item.name}
+                            currentPositionName={item.position}
+                            setItems={setItems}
+                            index={index}
+                            component={item.component}
+                        />
+                    </div>
                 ))}
             </div>
+            {currentItems?.find((item: any) => item.id === activeItem?.id)?.component}
         </div>
     )
 };
