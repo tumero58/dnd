22cbe6c1354c86544c82513213f4297b3b606b2b
tumero58 <erik.tumasyan@inomma.com>
2024-01-25 19:48:27 +0400

    feat: add redux

---
+++ src/redux/features/dndSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
+
+export type IDndState = {
+    dragging: boolean;
+};
+
+const initialState: IDndState = {
+    dragging: false
+};
+
+export const dndSlice = createSlice({
+    name: "dnd",
+    initialState,
+    reducers: {
+        changeDragging: (state: IDndState, action: { payload: boolean }) => {
+            state.dragging = action.payload
+        }
+    }
+});
+
+export const {
+    changeDragging
+} = dndSlice.actions;
+
+export const getDndDragging = (state: RootState) => state.dnd.dragging;
+
+export default dndSlice.reducer;
---
+++ src/redux/store.ts
@@ -0,0 +1,16 @@
+import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import dndReducer from "./features/dndSlice";
+
+export const store = configureStore({
+    reducer: {
+        dnd: dndReducer
+    },
+});
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+    ReturnType,
+    RootState,
+    unknown,
+    Action<string>
+>;
