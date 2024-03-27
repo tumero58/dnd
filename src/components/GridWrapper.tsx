47ea496f98d0c2e98bf0f2332a56df2b4e2c080e
Erik Tumasyan <erik.tumasyan@softconstruct.com>
2024-03-27 18:08:26 +0400

    feat: rendergriditems2

--- src/components/GridWrapper.tsx
+++ src/components/GridWrapper.tsx
@@ -1,6 +1,6 @@
 import { gridItemsDefault } from "@/utils/gridItems";
 import { Box } from "@mui/material";
-import { useState } from "react";
+import { useState, Fragment } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import GridItem from "./GridItem";
 
@@ -10,6 +10,27 @@ const GridWrapper = () => {
             ...gridItemsDefault
         ]
     })
+
+    const findMainComponents = (gridItems: any, items: any = [], direction: string = "horizontal", position: string = "", className: string = "") => {
+        const keys = Object.keys(gridItems);
+        for (let i = 0; i < keys.length; i++) {
+            if (keys[i].includes("main") && gridItems[keys[i]].length !== 0) {
+                items.push(
+                    {
+                        renderItem: gridItems[keys[i]],
+                        direction,
+                        className,
+                        position
+                    }
+                )
+            } else {
+                const dir = keys[i].includes("top") || keys[i].includes("bottom") ? "vetical" : "horizontal";
+                const position = keys[i].includes("top") || keys[i].includes("left") ? "before" : "";
+                findMainComponents(gridItems[keys[i]], items, dir, position, `${className}-${keys[i]}`)
+            }
+        }
+        return items;
+    }
 
     const renderGridItems = (gridItems: any, parentClassName: string = ""): any => {
         if (Object.keys(gridItems).length === 0) {
@@ -62,17 +83,70 @@ const GridWrapper = () => {
         )
     }
 
+    const renderGridItems2 = (gridItems: any) => {
+        const res = findMainComponents(gridItems);
+        console.log(res, "res");
+        const beforeRes: any = [];
+        const afterRes: any = [];
+        res.forEach((item: any) => {
+            if (item.position === "before") {
+                beforeRes.push(item)
+            } else {
+                afterRes.push(item)
+            }
+        })
+
+        const renderRes = [...beforeRes, ...afterRes];
+        return (
+            <>
+                {renderRes?.map((item: any, index: any) => {
+                    return (
+                        <Fragment key={index + 1}>
+                            <Panel minSize={20}>
+                                <GridItem className={item.className} items={item.renderItem} setItems={setGridItems} />
+                            </Panel>
+                            {index + 1 !== renderRes.length ? <PanelResizeHandle /> : <></>}
+                        </Fragment>
+                    )
+                })}
+            </>
+        )
+    }
+
+
     return (
-        <Box sx={{
-            width: "100%",
-            height: "100%",
-            border: "1px solid grey",
-            borderRadius: "4px",
-        }}>
-            <PanelGroup direction="horizontal">
-                {renderGridItems(gridItems)}
-            </PanelGroup>
-        </Box>
+        <>
+            <Box sx={{
+                padding: "16px",
+                height: "800px"
+            }}>
+                <Box sx={{
+                    width: "100%",
+                    height: "100%",
+                    border: "1px solid grey",
+                    borderRadius: "4px",
+                }}>
+                    <PanelGroup direction="horizontal">
+                        {renderGridItems(gridItems)}
+                    </PanelGroup>
+                </Box>
+            </Box>
+            <Box sx={{
+                padding: "16px",
+                height: "800px"
+            }}>
+                <Box sx={{
+                    width: "100%",
+                    height: "100%",
+                    border: "1px solid grey",
+                    borderRadius: "4px",
+                }}>
+                    <PanelGroup direction="horizontal">
+                        {renderGridItems2(gridItems)}
+                    </PanelGroup>
+                </Box>
+            </Box>
+        </>
     )
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
 import GridWrapper from "@/components/GridWrapper";
-import { Box } from "@mui/material";
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
 
 const Grid = () => {
     return (
         <DndProvider backend={HTML5Backend}>
-            <Box sx={{
-                padding: "16px",
-                height: "100vh"
-            }}>
-                <GridWrapper />
-            </Box>
+            <GridWrapper />
         </DndProvider>
     )
 };
