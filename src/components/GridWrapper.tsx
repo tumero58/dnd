8130ef5e097198a20c94dc49bdb5f2abfbf6660e
tumero58 <erik.tumasyan@inomma.com>
2024-05-24 17:20:24 +0400

    feat: add layouts

--- src/components/GridWrapper.tsx
+++ src/components/GridWrapper.tsx
@@ -1,5 +1,5 @@
 import { gridItemsDefault, orderGridItems } from "@/utils/gridItems";
-import { Box } from "@mui/material";
+import { Box, Button, TextField, Typography } from "@mui/material";
 import { Fragment, useCallback, useEffect, useState } from "react";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import GridItem from "./GridItem";
@@ -29,32 +29,72 @@ const GridWrapper = () => {
     }, [])
 
     const [renderReady, setRenderReady] = useState(false);
+    const [sizes, setSizes] = useState<any>({});
+    const [layoutName, setLayoutName] = useState("");
+    const [layouts, setLayouts] = useState([]);
+    const [activeLayout, setActiveLayout] = useState("");
 
     useEffect(() => {
-        const cachedItems = localStorage.getItem("items");
-        if (cachedItems) {
-            const parsed = JSON.parse(cachedItems || "");
-            setGridItems(syncComponents(parsed));
-            const sizes = localStorage.getItem("sizes");
-            const sizesParsed = JSON.parse(sizes || "");
-            setPrevSizes(sizesParsed);
-            setSizes(sizesParsed);
+        if (activeLayout) {
+            const layout = {
+                items: gridItems,
+                sizes
+            }
+            localStorage.setItem(activeLayout, JSON.stringify(layout))
         }
-        setRenderReady(true)
+    }, [gridItems, sizes, activeLayout])
 
+
+    const saveLayout = useCallback((name: string) => {
+        if (!name) {
+            return
+        }
+        setActiveLayout(name);
+        localStorage.setItem("activeLayout", name);
+        const prevLayouts = localStorage.getItem("layoutsList");
+        if (prevLayouts) {
+            const prevLayoutsParsed = JSON.parse(prevLayouts);
+            if (prevLayoutsParsed.includes(name)) {
+                return;
+            }
+            const newLayoutsList = [...prevLayoutsParsed, name];
+            localStorage.setItem("layoutsList", JSON.stringify(newLayoutsList));
+        } else {
+            const layoutsList = [name];
+            localStorage.setItem("layoutsList", JSON.stringify(layoutsList))
+        }
+        const layout = {
+            items: gridItems,
+            sizes
+        }
+        localStorage.setItem(name, JSON.stringify(layout))
+        const layoutsLocal = localStorage.getItem("layoutsList");
+        if (layoutsLocal) {
+            const layoutsParsed = JSON.parse(layoutsLocal);
+            setLayouts(layoutsParsed);
+        }
+        setOpenNewLayout(false);
+    }, [gridItems, sizes])
+
+    const loadLayout = useCallback((name: string) => {
+        const layout = localStorage.getItem(name);
+        if (layout) {
+            setActiveLayout(name);
+            localStorage.setItem("activeLayout", name);
+            const layoutParsed = JSON.parse(layout || "");
+            const { items, sizes } = layoutParsed;
+            setGridItems(syncComponents(items));
+            setSizes(sizes);
+        }
     }, [syncComponents])
 
     useEffect(() => {
-        localStorage.setItem("items", JSON.stringify(gridItems))
-    }, [gridItems]);
-
-
-    const [prevSizes, setPrevSizes] = useState<any>({});
-    const [sizes, setSizes] = useState({});
-
-    useEffect(() => {
-        localStorage.setItem("sizes", JSON.stringify(sizes))
-    }, [sizes]);
+        const activeLayoutLocal = localStorage.getItem("activeLayout");
+        if (activeLayoutLocal) {
+            loadLayout(activeLayoutLocal);
+        }
+        setRenderReady(true)
+    }, [loadLayout])
 
     const renderPanel = (res: any) => {
         return (
@@ -69,7 +109,7 @@ const GridWrapper = () => {
                         <Fragment key={index + 1}>
                             <Panel
                                 minSize={10}
-                                defaultSize={prevSizes[res.parentClassName || "main"]?.[index]}
+                                defaultSize={sizes[res.parentClassName || "main"]?.[index] || 100}
                             >
                                 {item.direction ?
                                     renderPanel(item) :
@@ -85,6 +125,15 @@ const GridWrapper = () => {
     }
 
     const orderedGridItems = orderGridItems(gridItems);
+    const [openNewLayout, setOpenNewLayout] = useState(false);
+
+    useEffect(() => {
+        const layoutsLocal = localStorage.getItem("layoutsList");
+        if (layoutsLocal) {
+            const layoutsParsed = JSON.parse(layoutsLocal);
+            setLayouts(layoutsParsed);
+        }
+    }, [])
 
     if (renderReady) {
         return (
@@ -93,6 +142,35 @@ const GridWrapper = () => {
                     padding: "16px",
                     height: "800px"
                 }}>
+                    <Box sx={{
+                        display: "flex",
+                        alignItems: "center"
+                    }}>
+                        {layouts?.length > 0 ?
+                            layouts.map((item: string, index: number) => {
+                                return (
+                                    <Button key={index + 1} onClick={() => {
+                                        loadLayout(item)
+                                    }}>{item}</Button>
+                                )
+                            }) : <></>}
+                        {openNewLayout ?
+                            <Box sx={{
+                                display: "flex",
+                                alignItems: "center"
+                            }}>
+                                <TextField onChange={(e) => { setLayoutName(e.target.value) }} />
+                                <Button onClick={() => { saveLayout(layoutName) }}>save layout</Button>
+                                <Button onClick={() => { setOpenNewLayout(false) }}>X</Button>
+                            </Box> :
+                            <Button onClick={() => { setOpenNewLayout(true) }}>+</Button>}
+                    </Box>
+                    <Box sx={{
+                        display: "flex",
+                        justifyContent: "center"
+                    }}>
+                        <Typography>{activeLayout}</Typography>
+                    </Box>
                     <Box sx={{
                         width: "100%",
                         height: "100%",
